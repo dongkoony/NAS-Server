74224b27198ccb703239984b2e7332efb7777fbd
donghyeonshin <donghyun4591@gmail.com>
2024-08-19 21:06:59 +0900

    대시보드 추가 예정

--- nas_app/views.py
+++ nas_app/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
@@ -12,11 +12,16 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('dashboard')  # 로그인 성공 후 대시보드로 리디렉션
+            messages.success(request, "로그인에 성공했습니다.")
+            return redirect('dashboard')
         else:
-            return HttpResponse("로그인 실패! 다시 시도하세요.")
+            messages.error(request, "로그인 실패! 사용자 이름과 비밀번호를 확인해주세요.")
     return render(request, 'login.html')
 
 @login_required
 def dashboard_view(request):
-    return HttpResponse("대시보드에 오신 것을 환영합니다!")
+    context = {
+        'username': request.user.username,
+        # 여기에 대시보드에 표시할 다른 데이터를 추가할 수 있습니다.
+    }
+    return render(request, 'dashboard.html', context)
