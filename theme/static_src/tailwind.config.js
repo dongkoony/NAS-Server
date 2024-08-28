1b6372e8f13ed134baefe7f7ee0ab24af2ebb780
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 09:19:16 +0900

    .

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,70 +1,56 @@
-/**
- * This is a minimal config.
- *
- * If you need the full config, get it from here:
- * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
- */
+// ./theme/static_src/tailwind.config.js
+
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    /**
-     * Stylesheet generation mode.
-     *
-     * Set mode to "jit" if you want to generate your styles on-demand as you author your templates;
-     * Set mode to "aot" if you want to generate the stylesheet in advance and purge later (aka legacy mode).
-     */
-    mode: "aot",
-
-    purge: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
-
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+    content: [
         '../templates/**/*.html',
-
-        /* 
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
         '../../templates/**/*.html',
-        
-        /* 
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
-
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
-
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
+        '!../../node_modules/**/*',
+        '!../../**/node_modules/**/*',
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'media',
     theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                body: ['Inter var', ...defaultTheme.fontFamily.sans],
+                heading: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                gray: defaultTheme.colors.gray,
+                blue: defaultTheme.colors.blue,
+                red: defaultTheme.colors.red,
+                yellow: defaultTheme.colors.yellow,
+                green: defaultTheme.colors.green,
+            },
+            borderRadius: {
+                'xl': `calc(var(--radius) + 4px)`,
+                'lg': `var(--radius)`,
+                'md': `calc(var(--radius) - 2px)`,
+                'sm': `calc(var(--radius) - 4px)`,
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
     },
     plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+        require("tailwindcss-animate"),
     ],
-}
+};
