d05a0154f7106f4c7b06a2042b57721fe56ee21c
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 14:09:15 +0900

    messages 추가

--- nas_app/views.py
+++ nas_app/views.py
@@ -1,13 +1,15 @@
 # ./nas_app/views.py
 
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 from django.conf import settings
-import os
+from django.contrib import messages
+
 from .models import NASFile
-from .forms import FileUploadForm
+from .forms import MultipleFileUploadForm
 
 def login_view(request):
     if request.method == 'POST':
@@ -16,40 +18,30 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, '로그인에 성공했습니다.')
             return redirect('dashboard')
         else:
-            return render(request, 'login.html', {'error': '아이디 또는 비밀번호가 올바르지 않습니다.'})
+            messages.error(request, '아이디 또는 비밀번호가 올바르지 않습니다.')
     return render(request, 'login.html')
 
 @login_required(login_url='/')
 def dashboard_view(request):
     if request.method == 'POST':
-        print(request.FILES)
-        form = FileUploadForm(request.POST, request.FILES)
+        form = MultipleFileUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            nas_file = form.save()
-            print(f"File saved at: {nas_file.file.path}")  # 저장된 파일의 경로를 출력
-            print(f"MEDIA_ROOT is set to: {settings.MEDIA_ROOT}")
+            files = request.FILES.getlist('files')
+            description = form.cleaned_data['description']
+            for file in files:
+                NASFile.objects.create(file=file, description=description)
+            messages.success(request, f'{len(files)}개의 파일이 성공적으로 업로드되었습니다.')
             return redirect('dashboard')
         else:
-            print("Form is not valid.")
-            print(form.errors)  # 폼 오류를 출력하여 문제를 진단
+            messages.error(request, '파일 업로드 중 오류가 발생했습니다. 다시 시도해주세요.')
     else:
-        form = FileUploadForm()
+        form = MultipleFileUploadForm()
     
     files = NASFile.objects.all()
     return render(request, 'dashboard.html', {'form': form, 'files': files})
-
-@login_required(login_url='/')
-def file_upload_view(request):
-    if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('file_list')
-    else:
-        form = FileUploadForm()
-    return render(request, 'file_upload.html', {'form': form})
 
 @login_required(login_url='/')
 def file_list_view(request):
