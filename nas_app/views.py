233f250a915b79f3a44f665596706fd80b881df9
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 10:24:13 +0900

    아이디 저장 기능 추가

--- nas_app/views.py
+++ nas_app/views.py
@@ -1,27 +1,28 @@
 # ./nas_app/views.py
 
+# ./nas_app/views.py
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, "로그인에 성공했습니다.")
-            return redirect('dashboard')
+            print(f"User {user.username} logged in successfully.")
+            return redirect('dashboard')  # 로그인 성공 후 대시보드로 리디렉션
         else:
-            messages.error(request, "로그인 실패! 사용자 이름과 비밀번호를 확인해주세요.")
+            print("Authentication failed.")
+            # 로그인 실패 시 오류 메시지와 함께 로그인 페이지를 다시 렌더링
+            return render(request, 'login.html', {'error': '아이디 또는 비밀번호가 올바르지 않습니다.'})
     return render(request, 'login.html')
 
-@login_required
+
+@login_required(login_url='/')
 def dashboard_view(request):
-    context = {
-        'username': request.user.username,
-        # 여기에 대시보드에 표시할 다른 데이터를 추가할 수 있습니다.
-    }
-    return render(request, 'dashboard.html', context)
+    # 대시보드에 필요한 데이터를 추가할 수 있습니다.
+    return render(request, 'dashboard.html')
