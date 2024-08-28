7a6a975a59feb32ee136de2963519a2d3263d96d
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 14:08:59 +0900

    다수의 파일 업로드 폼

--- nas_app/forms.py
+++ nas_app/forms.py
@@ -1,9 +1,32 @@
 # ./nas_app/forms.py
 
 from django import forms
-from .models import NASFile
+from django.forms.widgets import ClearableFileInput
 
-class FileUploadForm(forms.ModelForm):
-    class Meta:
-        model = NASFile
-        fields = ['file', 'description']
+class MultipleFileInput(ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+class MultipleFileUploadForm(forms.Form):
+    files = MultipleFileField(
+        label='파일 선택',
+        required=True
+    )
+    description = forms.CharField(
+        max_length=255,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '파일 설명 (선택사항)'}),
+        label='설명'
+    )
