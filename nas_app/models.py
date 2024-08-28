a25fa92b46d7878ab8e068dbfd614514e993329e
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 12:23:38 +0900

    메타데이터 저장

--- nas_app/models.py
+++ nas_app/models.py
@@ -1,3 +1,11 @@
+# ./nas_app/models.py
+
 from django.db import models
 
-# Create your models here.
+class NASFile(models.Model):
+    file = models.FileField(upload_to='')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.file.name
