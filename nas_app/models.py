d3a657274d5ba696e64398a84935b9af4315bbec
donghyeonshin <donghyun4591@gmail.com>
2024-08-28 12:49:13 +0900

    upload_to 속성 추가

--- nas_app/models.py
+++ nas_app/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 class NASFile(models.Model):
-    file = models.FileField(upload_to='')
+    file = models.FileField(upload_to='', storage=None)  # `upload_to` 속성을 비워 두면 기본적으로 MEDIA_ROOT에 저장됩니다.
     uploaded_at = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=255, blank=True)
 
